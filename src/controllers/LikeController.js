47285cc6584bb3169097ede709d8c9483c7f8cbe
Kelvyn Santana <kelvyn.santana@icloud.com>
2019-08-15 21:20:04 -0300

    imprementaçãpo de Websockets para função match

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -3,6 +3,8 @@ const Dev = require('../models/Dev');
 
 module.exports = { 
     async store(req, res){
+        console.log(req.io, req.connectedUsers)
+
         const { user } = req.headers;
         const { devId } = req.params;
         
@@ -14,9 +16,18 @@ module.exports = {
             return res.status(400).json({ error: "Dev not exists"});
         };
 
+        //MATCH
         if(targetDev.likes.includes(loggedDev._id)) {
-            console.log('DEU MATCH')
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
 
+            if(loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if(targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         };
         
         loggedDev.likes.push(targetDev._id);
